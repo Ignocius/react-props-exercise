faae91ece1913a03ea473ab0ed0053a1611e4fbd
ignocius <ignocius@gmail.com>
2019-05-15 11:10:51 +0200

    commentdetail created

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">12.00Pm</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
     </div>
   );
 }
